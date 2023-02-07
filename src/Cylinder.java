8e2e9273731ca5e9cf736c8678c8f0eb0da46229
Michael <mblakey2@ggc.edu>
2023-02-07 17:51:55 -0500

    add Cylinder.java

---
+++ src/Cylinder.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Cylinder {
+
+    public static void main(String[]args){
+       final double PI = 3.1416;
+
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the radius of base circle:");
+       double radius = input.nextDouble();
+
+
+        System.out.print("Enter the length of the cylinder:");
+        double length  = input.nextDouble();
+
+       double base_area = radius * PI;
+
+       double volume = length *PI;
+
+       System.out.println(radius + " radius = " + base_area + " base area ");
+
+       System.out.println(length + " Length = " + volume + " volume ");
+
+
+
+
+
+
+    }
+}
