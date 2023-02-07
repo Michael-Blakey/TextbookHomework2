dfffc660931bcb34f2f05b5fb84aad3011e6d2cc
Michael <mblakey2@ggc.edu>
2023-02-07 18:00:34 -0500

    add Cylinder.java

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Cylinder {
 
     public static void main(String[]args){
-       final double PI = 3.1416;
+       final double PI = 3.1416; //Number given in textbook example
 
        Scanner input = new Scanner(System.in);
-       System.out.print("Enter the radius of base circle:");
-       double radius = input.nextDouble();
+       System.out.print("Enter the radius of base circle:"); // any given number will appear as wanted.
+       double radius = input.nextDouble(); // variable we need to get the base area.
 
 
         System.out.print("Enter the length of the cylinder:");
